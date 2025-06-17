e4799d75164bfe46dc5a8b8da8a9195dc7bac556
AdryanBot <adryanblueberry@gmail.com>
2025-06-17 19:52:19 -0300

    Correções no TabelaUtils

--- src/main/java/main/TabelaUtils.java
+++ src/main/java/main/TabelaUtils.java
@@ -8,7 +8,14 @@ import java.util.function.Function;
 public class TabelaUtils {
 
     public static <T> JScrollPane gerarTabela(String[] colunas, List<T> dados, Function<T, Object[]> mapeadorLinha) {
-        DefaultTableModel modelo = new DefaultTableModel(colunas, 0);
+        // Subclasse anônima do DefaultTableModel que impede edição
+        DefaultTableModel modelo = new DefaultTableModel(colunas, 0) {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false; // Impede edição de qualquer célula
+            }
+        };
+
         for (T item : dados) {
             modelo.addRow(mapeadorLinha.apply(item));
         }
